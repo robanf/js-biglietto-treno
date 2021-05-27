779c2747e8b28b8ca7d6bc6eedc4130d13886ab5
Roberto <robertoanfuso98@gmail.com>
2021-05-27 16:23:00 +0200

    fine esercizio

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,24 @@
 var eta=prompt("Inserisci la tua età");
+
+var km=prompt("quanti km devi fare?");
+
+var prezzo=km*0.21;
+
+parseFloat(prezzo);
+
+console.log(prezzo);
+
+
+if(eta<18){
+
+    prezzo=prezzo*0.8;
+    document.getElementById('prezzof').innerHTML= "il tuo prezzo sarà="+prezzo;
+
+}else if(eta>65) {
+
+    prezzo=prezzo*0.6;
+    document.getElementById('prezzof').innerHTML= "il tuo prezzo sarà="+prezzo;
+
+}else {
+    document.getElementById('prezzof').innerHTML= "il tuo prezzo sarà="+prezzo;
+}
